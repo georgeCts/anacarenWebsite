0cfc1cacf17e6ed99a610e29ede8f577272e37fe
Jorge Cortés <georgeluis.idem@gmail.com>
2021-09-28 20:31:24 -0500

    Coming soon

---
+++ resources/views/construction/index.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Website developer">
+    <meta name="author" content="George Cortes">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="Anacaren Gutierrez" /> <!-- website name -->
+	<meta property="og:site" content="http://anacarengutierrez.com" /> <!-- website link -->
+	<meta property="og:title" content="Anacaren Gutierrez"/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="Blog y tienda de anacaren, entra y sigue los mejores consejos de viaje y más!" /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="?_nc_ht=instagram.fmid2-1.fna.fbcdn.net&_nc_ohc=X_9kW3Kg5SgAX-WfJ3y&edm=ABfd0MgBAAAA&ccb=7-4&oh=c8957e7c72ba9a6ed96be6580205ade5&oe=615B6531&_nc_sid=7bff83" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="https://www.instagram.com/anac_gtz/" /> <!-- where do you want your post to link to -->
+	<meta name="twitter:card" content="summary_large_image"> <!-- to have large image post format in Twitter -->
+
+    <!-- Webpage Title -->
+    <title>Anacaren Gutierrez</title>
+    
+    <!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,700&display=swap" rel="stylesheet" />
+    <link href="{{ asset('construction/css/bootstrap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('construction/css/all.css') }}" rel="stylesheet" />
+	<link href="{{ asset('construction/css/styles.css') }}" rel="stylesheet" />
+	
+	<!-- Favicon  -->
+    <link rel="icon" href="{{ asset('construction/images/favicon.png') }}" />
+</head>
+<body>
+    
+    <!-- Header -->
+    <header id="header" class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-container">
+                        <div class="countdown">
+                            <span id="clock"></span>
+                        </div> <!-- end of countdown -->
+
+                        <h1>Próximamente</h1>
+                        <p class="p-large">En este sitio estamos construyendo cosas chingonas...</p>
+
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+
+        <!-- Social Links -->
+        <div class="social-container">
+            <a class="logo-text" href="#">Blog, tienda y más!</a>
+
+            <span class="fa-stack">
+                <a href="https://www.facebook.com/anacarengtz">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-facebook-f fa-stack-1x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://twitter.com/anacarengtz">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-twitter fa-stack-1x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://www.youtube.com/channel/UCJHJSFl1Fm-8EKMqdEoO9MQ">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-youtube fa-stack-1x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://www.instagram.com/anac_gtz/">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-instagram fa-stack-1x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://www.tiktok.com/@anac_gtz">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-tiktok fa-stack-1x"></i>
+                </a>
+            </span>
+        </div> <!-- end of social-container -->
+        <!-- end of social links -->
+    </header> <!-- end of header -->
+    <!-- end of header -->
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('construction/js/jquery.min.js') }}"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="{{ asset('construction/js/bootstrap.min.js') }}"></script> <!-- Bootstrap framework -->
+    <script src="{{ asset('construction/js/jquery.easing.min.js') }}"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="{{ asset('construction/js/jquery.countdown.min.js') }}"></script> <!-- The Final Countdown plugin for jQuery -->
+    <script src="{{ asset('construction/js/scripts.js') }}"></script> <!-- Custom scripts -->
+</body>
+</html>
