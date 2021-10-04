9b70c0b1646f69c5e84506aa7974c855fa32df4e
Jorge Cortés <georgeluis.idem@gmail.com>
2021-10-03 21:36:35 -0500

    storage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UsersSeeder::class,
+            UsersSeeder::class,
             CategoriesSeeder::class,
         ]);
     }
